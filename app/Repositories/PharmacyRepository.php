4f95ea56e45de729084305068d06babf39e32d3f
Habiba <habebaesam13@gmail.com>
2025-12-29 15:48:45 +0200

    repos logic added

--- app/Repositories/PharmacyRepository.php
+++ app/Repositories/PharmacyRepository.php
@@ -2,13 +2,46 @@
 
 namespace App\Repositories;
 
+use App\Models\Pharmacy;
+
 class PharmacyRepository
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    protected $model;
+    public function __construct(Pharmacy $product)
     {
-        //
+        $this->model=$product;
+    }
+    public function all(){
+        return $this->model->all();
+    }
+
+    public function find($id){
+        return $this->model->findOrFail($id);
+    }
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $product = $this->model->find($id);
+        if ($product) {
+            $product->update($data);
+            return $product;
+        }
+        return null;
+    }
+
+    public function delete($id)
+    {
+        $product = $this->model->find($id);
+        if ($product) {
+            return $product->delete();
+        }
+        return false;
     }
 }
--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -2,13 +2,49 @@
 
 namespace App\Repositories;
 
-class ProductRepository
+use App\Models\Product;
+use App\Contracts\BaseRepository;
+
+
+class ProductRepository implements BaseRepository
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    protected $model;
+    public function __construct(Product $product)
     {
-        //
+        $this->model=$product;
     }
+    public function all(){
+        return $this->model->all();
+    }
+
+    public function find($id){
+        return $this->model->findOrFail($id);
+    }
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $product = $this->model->find($id);
+        if ($product) {
+            $product->update($data);
+            return $product;
+        }
+        return null;
+    }
+
+    public function delete($id)
+    {
+        $product = $this->model->find($id);
+        if ($product) {
+            return $product->delete();
+        }
+        return false;
+    }
+
 }
